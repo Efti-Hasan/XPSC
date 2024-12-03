b2b2f488b39f7d03e337886be2767d216e11e7ea
Efti Hasan <92861990+Efti-Hasan@users.noreply.github.com>
2024-12-04 00:01:08 +0600

    Update A_Bazoka_and_Mocha_s_Array.cpp

--- Week6/Day1/A_Bazoka_and_Mocha_s_Array.cpp
+++ Week6/Day1/A_Bazoka_and_Mocha_s_Array.cpp
@@ -13,9 +13,9 @@ int main()
     {
         int n;
         cin>>n;
-        for(int i=0;i<n;i++) cin>>arr[i];
+        for(int i=1;i<=n;i++) cin>>arr[i];
         int pos=0;
-        for(int i=0;i<n;i++)
+        for(int i=1;i<=n;i++)
         {
             if(arr[i]>arr[i+1])
             {
